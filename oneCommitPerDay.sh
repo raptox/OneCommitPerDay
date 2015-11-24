86c952d6af70bbc0304eff6484af5761bc90c19f
Alexander Tornoreanu <alex@tornoreanu.ro>
2015-11-24 21:11:53 +0000

    adapted script

--- oneCommitPerDay.sh
+++ oneCommitPerDay.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
+
+# change this to the path of the project directory where you want to make daily commits
+project_directory="/home/pi/work/OneCommitPerDay/"
 
 commit_file="commitFile.txt"
 commit_message=`w3m -dump http://whatthecommit.com | head -1`
 
-# create commit_file if it does not exist
-if [ ! -f $commit_file ] ; then
-	touch $commit_file
-fi
-
 # add new content to file, commit and push
+cd $project_directory
 echo "$commit_message on `date`" >> $commit_file
 git add $commit_file
 git commit -m "$commit_message"
